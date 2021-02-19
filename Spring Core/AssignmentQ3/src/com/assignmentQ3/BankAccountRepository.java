163e839b295d5c443de1f9074361d2ba8475d26a
Abhishek Jha <75085546+cyborg-aj-3069@users.noreply.github.com>
2021-02-19 22:42:31 +0530

    Delete Spring Core directory

--- Spring Core/AssignmentQ3/src/com/assignmentQ3/BankAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.assignmentQ3;
-
-public interface BankAccountRepository {
-	
-	public double getBalance(long accountId);
-	public double updateBalance(long accountId, double newBalance);
-
-}
